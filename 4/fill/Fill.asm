fbce44e34f2a9db8ec91156b606c2775ef842977
Marc <marckarella13@gmail.com>
2025-11-07 11:54:05 -0800

    INitial commit of completed files for mult.asm and fill.asm: both files tested and are functional.

--- 4/fill/Fill.asm
+++ 4/fill/Fill.asm
@@ -8,4 +8,44 @@
 // i.e. writes "black" in every pixel. When no key is pressed, 
 // the screen should be cleared.
 
-//// Replace this comment with your code.
+(MAIN)
+@KBD
+D=M
+@BLACK
+D;JGT
+@WHITE
+D;JEQ
+(BLACK)
+D=-1
+@FILL
+0;JMP
+(WHITE)
+D=0
+@FILL
+0;JMP
+(FILL)
+@COLOR
+M=D
+@SCREEN
+D=A
+@POINTER
+M=D
+@8192
+D=D+A
+@END
+M=D
+(LOOP)
+@COLOR
+D=M
+@POINTER
+A=M
+M=D
+@POINTER
+M=M+1
+D=M
+@END
+D=D-M
+@LOOP
+D;JLT
+@MAIN
+0;JMP
