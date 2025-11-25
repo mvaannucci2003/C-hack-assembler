119d487d938865f9acc0f4395a80f1f1ed1716f2
Marc V. <marckarella13@gmail.com>
2025-11-24 16:51:51 -0800

    started hack file and saving progress.

---
+++ cplorations/c09/hack.h
@@ -0,0 +1,29 @@
+#define NUM_PREDEFINED_SYMBOLS 23
+
+typedef enum symbol_id
+{
+    SYM_R0 = 0,
+    SYM_R1 = 1,
+    SYM_R2 = 2,
+    SYM_R3 = 3,
+    SYM_R4 = 4,
+    SYM_R5 = 5,
+    SYM_R6 = 6,
+    SYM_R7 = 7,
+    SYM_R8 = 8,
+    SYM_R9 = 9,
+    SYM_R10 = 10,
+    SYM_R11 = 11,
+    SYM_R12 = 12,
+    SYM_R13 = 13,
+    SYM_R14 = 14,
+    SYM_R15 = 15,
+    SYM_R16 = 16,
+    SYM_R17 = 17,
+    SYM_R18 = 18,
+    SYM_R19 = 19,
+    SYM_R20 = 20,
+    SYM_R21 = 21,
+    SYM_R22 = 22,
+
+} symbol_id;
